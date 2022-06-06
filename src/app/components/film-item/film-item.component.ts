fbde740efe3932041e6921e678b23b85aef4301d
Reshetnik-Eugenius <Eugenius@example.com>
2022-06-07 00:15:34 +0300

    temp point 2

--- src/app/components/film-item/film-item.component.ts
+++ src/app/components/film-item/film-item.component.ts
@@ -4,6 +4,7 @@ import { FilmService } from 'src/app/shared/service/film.service';
 // import { FilmDetailsComponent } from '../film-details/film-details.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from '../modal/modal.component';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-film-item',
@@ -14,37 +15,57 @@ export class FilmItemComponent implements OnInit {
   constructor(private filmService: FilmService, public dialog: MatDialog) { }
   // private dialog: MatDialog,
   ngOnInit(): void {
+    // this.getMovieById('tt0318403');
   }
 
   @Input() film!: IFilm; 
 
-  getMovieById(id:string): void {
-    this.filmService.getFilmById(id).subscribe((film: IFilmDetails) => {
-      // this.datas = datas;
-      // this.film = film;
+      // columns we will show on the table
+  public displayedColumns = ['firstName', 'lastName'];
+  //the source where we will get the data
+  public dataSource = new MatTableDataSource<IFilm>();
 
-      this.showSelectMovie(film);
-      this.openDialog();
-      // console.log(film);
-  }) 
+  // getMovieById(id:string): void {
+  //   this.filmService.getFilmById(id)
+  //     .subscribe((film: IFilmDetails) => {
+  //     // this.datas = datas;
+  //     // this.film = film;
+
+  //     this.showSelectMovie(film);
+  //     this.openDialog();
+  //     // console.log(film);
+  // }) 
+  getMovieById(id:string): void {
+    this.filmService.getFilmById(id)
+    .subscribe((res)=>{
+      console.log(res);
+      this.dataSource.data = res;
+    })
+    this.openDialog();
+  }
+  openDialog() {
+    // throw new Error('Method not implemented.');
+    console.log("asd");
+    this.dialog.open(ModalComponent);
+  }
 }
-openDialog() {
-  console.log("asd");
-  this.dialog.open(ModalComponent);
-}
+// openDialog() {
+//   console.log("asd");
+//   this.dialog.open(ModalComponent);
+// }
 // getFilmById
-showSelectMovie(movie: IFilmDetails) {
+// showSelectMovie(movie: IFilmDetails) {
     
-    let filmView = {
-        Title: movie.Title,
-        Released: movie.Released,
-        Gentre: movie.Genre,
-        Country: movie.Country,
-        Director: movie.Director,
-        Writer: movie.Writer,
-        Actors: movie.Actors,
-        Awards: movie.Awards,
-    };
+    // let filmView = {
+    //     Title: movie.Title,
+    //     Released: movie.Released,
+    //     Gentre: movie.Genre,
+    //     Country: movie.Country,
+    //     Director: movie.Director,
+    //     Writer: movie.Writer,
+    //     Actors: movie.Actors,
+    //     Awards: movie.Awards,
+    // };
 
     // while(tableFilmInfo.rows.length) {
     //     tableFilmInfo.deleteRow(0);
@@ -63,6 +84,6 @@ showSelectMovie(movie: IFilmDetails) {
 
     //     filmInfoImg.src = movie.Poster; 
     // }
-}
+// }
 
-} 
+// } 
